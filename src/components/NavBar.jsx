a52a73c52d29c7f55a27d6ab67bcf78beaaa7da6
Bridget <bridget.tang@yahoo.com>
2026-01-28 20:16:32 +1000

    complete template

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
-import { Outlet } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import "./NavBar.css";
 
 function NavBar() {
     return (
         <nav>
-            <Link to="/"> Home </Link>
-            <Link to="/fundraiser"> Fundraiser</Link>
+            <ul className="nav-list">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/fundraiser">Fundraiser</Link></li>
+            </ul>
         </nav>
     );
 }
