f4e24a38712bcb83ca62f5fe9e19c42e6faa32fa
Bridget <bridget.tang@yahoo.com>
2026-01-28 19:25:15 +1000

    Initial commit

---
+++ src/pages/FundraiserPage.jsx
@@ -0,0 +1,5 @@
+function FundraiserPage() {
+    return <h1> this is the FundraiserPage</h1>
+}
+
+export default FundraiserPage;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+// display all fundraisers
+import { allFundraisers } from "../data";
+
+
+
+function HomePage() {
+    return (
+        <div>
+            {allFundraisers.map((fundraiserData, key) => {
+                return <div key={key}>fundraiserData.title</div>;
+                  
+                );
+            }
+
+        </div>
+    );
+}
+
+export default HomePage;
