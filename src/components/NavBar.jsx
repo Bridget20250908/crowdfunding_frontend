385f526222cc310241d6acecb046f6ec06b02b49
Bridget <bridget.tang@yahoo.com>
2026-02-05 11:11:14 +1000

    Add pledge operations

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,19 +6,27 @@ import "./NavBar.css";
 function NavBar() {
     const { isLoggedIn, user } = useContext(AuthContext);
     return (
-        <nav>
-            <ul className="nav-list">
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/about">About</NavLink></li>
-                <li><NavLink to="/contact">Contact</NavLink></li>
-                {!isLoggedIn ? (
-                  <li><NavLink to="/login">Log In</NavLink></li>
-                ) : (
-                  <li><span>Welcome, {user?.username}</span></li>
-                  <li><NavLink to="/logout">Log Out</NavLink></li>
-                )}
-            </ul>
-        </nav>
+        <div className="nav-bar">
+            <nav>
+                <ul className="nav-list">
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
+                </ul>
+            </nav>
+            <nav>
+                <ul className="nav-list">
+                    {!isLoggedIn ? (
+                      <li><NavLink to="/login">Log In</NavLink></li>
+                    ) : (
+                        <div className="login-container">
+                            <li><span>{user?.username}</span></li>
+                            <li><span>Log Out</span></li>
+                        </div>
+                    )}
+                </ul>
+            </nav>
+        </div>
     );
 }
 
